d43a17f6db6957fe6f400e6952965ce852f73b16
Mindula-Dilthushan <minduladilthushan1@gmail.com>
2021-05-21 14:02:33 +0530

    console print name

---
+++ src/ts/mainFunctions.js
@@ -0,0 +1,4 @@
+var character = 'Mindula Dilthushan';
+console.log(character);
+var inputs = document.querySelectorAll('input');
+console.log(inputs);
---
+++ src/ts/mainFunctions.ts
@@ -0,0 +1,5 @@
+const character = 'Mindula Dilthushan';
+console.log(character);
+
+const inputs = document.querySelectorAll('input');
+console.log(inputs);
