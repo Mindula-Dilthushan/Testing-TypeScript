228fc3f2f461285464e7260aa7248d4a13718118
Mindula-Dilthushan <minduladilthushan1@gmail.com>
2021-05-21 14:09:32 +0530

    forEach Loops added

--- src/ts/mainFunctions.js
+++ src/ts/mainFunctions.js
@@ -2,3 +2,6 @@ var character = 'Mindula Dilthushan';
 console.log(character);
 var inputs = document.querySelectorAll('input');
 console.log(inputs);
+inputs.forEach(function (input) {
+    console.log(input);
+});
--- src/ts/mainFunctions.ts
+++ src/ts/mainFunctions.ts
@@ -3,3 +3,7 @@ console.log(character);
 
 const inputs = document.querySelectorAll('input');
 console.log(inputs);
+
+inputs.forEach(input =>{
+    console.log(input);
+})
