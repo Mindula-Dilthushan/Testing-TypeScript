b65c84c51c6f0f11a2b4c9664b02f5013d88afc3
Mindula-Dilthushan <minduladilthushan1@gmail.com>
2021-05-31 12:59:44 +0530

    object to added

--- src/04_Explicit_&_Union_Types/Explicit.ts
+++ src/04_Explicit_&_Union_Types/Explicit.ts
@@ -15,3 +15,22 @@ Iam.push('mindula');
 Iam.push(20);
 Iam.push(false);
 console.log(Iam);
+
+//Objects
+let me : object;
+me = {
+    name : 'Mindula',
+    age : 21
+};
+
+let me1 : {
+    name : string,
+    age : number
+}
+me1 = {
+    name : 'Dilthushan',
+    age : 21
+}
+
+console.log(me);
+console.log(me1);
