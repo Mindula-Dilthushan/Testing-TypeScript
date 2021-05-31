6bba4fd884fa4cda8ebcbcd85a31b852685af8c1
Mindula-Dilthushan <minduladilthushan1@gmail.com>
2021-05-31 12:55:59 +0530

    Explicit Added

---
+++ src/04_Explicit_&_Union_Types/Explicit.ts
@@ -0,0 +1,17 @@
+//explicit types
+
+let character : string;
+let age : number;
+let isSignIn : boolean;
+
+character = "Mindula";
+age = 21;
+isSignIn = true;
+
+//Arrays
+
+let Iam : (string|number|boolean)[] = [];
+Iam.push('mindula');
+Iam.push(20);
+Iam.push(false);
+console.log(Iam);
